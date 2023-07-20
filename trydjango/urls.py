6c07346e63f3db4ae1e2e2dd93f76ce38f04fcad
newwohh <nevoznvx@gmail.com>
2023-07-20 17:46:11 +0530

    added-dashboard

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',include('core.urls')),
     path('item/',include('item.urls')),
     path('dashboard/',include('dashboard.urls')),
+        path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
 ]
